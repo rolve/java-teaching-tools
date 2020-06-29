4514fcb2b1aeb866874479e02a86066a646c76bf
Michael Faes <rolve@trick17.ch>
2020-06-29 15:37:04 +0200

    Test detection of nondeterminism

--- grader/src/test/java/ch/trick17/jtt/grader/GraderTest.java
+++ grader/src/test/java/ch/trick17/jtt/grader/GraderTest.java
@@ -249,6 +249,21 @@ public class GraderTest {
         assertEquals(expected, results);
     }
 
+    @Test
+    public void testNondeterminism() throws IOException {
+        var tasks = List.of(new Task("AddTest", "Add"));
+        var grader = new Grader(tasks, ECLIPSE_ROOT,
+                ProjectStructure.ECLIPSE, Compiler.ECLIPSE);
+        grader.gradeOnly("0", "8");
+        grader.run();
+        var results = readAllLines(Path.of("results-AddTest.tsv"));
+        var expected = List.of(
+                "Name\tnondeterministic\ttestAdd1\ttestAdd2",
+                "0\t0\t1\t1",
+                "8\t1\t0\t0");
+        assertEquals(expected, results);
+    }
+
     @AfterAll
     public static void deleteLogFiles() throws IOException {
         try (var allFiles = list(Path.of("."))) {
