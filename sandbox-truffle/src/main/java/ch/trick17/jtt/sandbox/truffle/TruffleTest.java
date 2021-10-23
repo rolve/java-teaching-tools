3424de2cfe547e29a91d104e99e656337a80792d
Michael Faes <rolve@trick17.ch>
2021-10-23 18:27:35 +0200

    Some performance experiments

--- sandbox-truffle/src/main/java/ch/trick17/jtt/sandbox/truffle/TruffleTest.java
+++ sandbox-truffle/src/main/java/ch/trick17/jtt/sandbox/truffle/TruffleTest.java
@@ -2,20 +2,50 @@ package ch.trick17.jtt.sandbox.truffle;
 
 import org.graalvm.polyglot.Context;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class TruffleTest {
     public static void main(String[] args) {
-        var context = Context.newBuilder()
-                .allowNativeAccess(true)
-                .allowCreateThread(true)
-                .option("java.Classpath", System.getProperty("java.class.path"))
-                .build();
+        for (int i = 0; i < 10; i++) {
+            var start = System.nanoTime();
 
-        context.getBindings("java").getMember(Guest.class.getName()).invokeMember("main", (Object) null);
+            var context = Context.newBuilder()
+                    .allowNativeAccess(true)
+                    .allowCreateThread(true)
+                    .option("java.Classpath", System.getProperty("java.class.path"))
+                    .build();
+            context.getBindings("java").getMember(Guest.class.getName())
+                    .invokeMember("main", (Object) null);
+
+            var time = (System.nanoTime() - start) / 1_000_000_000.0;
+            System.out.printf("%.2f s (host)\n", time);
+        }
     }
 
     public static class Guest {
+
         public static void main(String[] args) {
-            System.out.println("Hello, World!");
+            var start = System.nanoTime();
+
+            var random = new Random();
+            IntStream.generate(() -> random.nextInt(1_000_000))
+                    .filter(Guest::isPrime)
+                    .limit(1000)
+                    .forEach(System.out::println);
+
+            var time = (System.nanoTime() - start) / 1_000_000_000.0;
+            System.out.printf("\n%.2f s (guest)\n", time);
+        }
+
+        public static boolean isPrime(int number) {
+            int sqrt = (int) Math.sqrt(number) + 1;
+            for (int i = 2; i < sqrt; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
