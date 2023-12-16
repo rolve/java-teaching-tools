dc536c5748e4cb154c623f5503357e53d65a455e
Michael Faes <rolve@trick17.ch>
2023-12-16 13:34:03 +0100

    Try class loading features of Javassist

--- sandbox/src/test/java/ch/trick17/jtt/sandbox/JavassistTest.java
+++ sandbox/src/test/java/ch/trick17/jtt/sandbox/JavassistTest.java
@@ -1,29 +1,40 @@
 package ch.trick17.jtt.sandbox;
 
-import javassist.CannotCompileException;
-import javassist.ClassPool;
+import javassist.*;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
+
+import static java.lang.Thread.currentThread;
 
 public class JavassistTest {
     public static void main(String[] args) throws Exception {
         var pool = ClassPool.getDefault();
-        var cls = pool.get("ch.trick17.jtt.sandbox.ClassUnderTest");
-        var foo = cls.getDeclaredMethod("foo");
-        foo.instrument(new ExprEditor() {
-            public void edit(MethodCall m) throws CannotCompileException {
-                if (m.getClassName().equals("java.lang.Math") &&
-                        m.getMethodName().equals("sin")) {
-                    m.replace("""
-                            {
-                                System.out.println("sin");
-                                $_ = $proceed($$);
-                            }""");
+        var loader = new Loader(pool);
+        loader.addTranslator(pool, new Translator() {
+            public void start(ClassPool pool) {}
+            public void onLoad(ClassPool pool, String classname) throws NotFoundException, CannotCompileException {
+                var cls = pool.get(classname);
+                for (var method : cls.getDeclaredMethods()) {
+                    method.instrument(new ExprEditor() {
+                        public void edit(MethodCall m) throws CannotCompileException {
+                            if (m.getClassName().equals("java.lang.Math") &&
+                                m.getMethodName().equals("sin")) {
+                                m.replace("""
+                                        {
+                                            System.out.println("sin");
+                                            $_ = $proceed($$);
+                                        }""");
+                            }
+                        }
+                    });
                 }
             }
         });
-        cls.toClass(JavassistTest.class);
+        var runner = new CustomCxtClassLoaderRunner(loader);
 
-        ClassUnderTest.foo();
+        runner.call(() -> {
+            var cls = currentThread().getContextClassLoader().loadClass("ch.trick17.jtt.sandbox.ClassUnderTest");
+            return cls.getMethod("foo").invoke(null);
+        });
     }
 }
