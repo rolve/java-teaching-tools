dae24eefe6e24d20678bca7e0f4f33f4fa26c834
Michael Faes <rolve@trick17.ch>
2023-12-16 13:54:12 +0100

    Simple whitelist functionality

--- sandbox/src/test/java/ch/trick17/jtt/sandbox/JavassistTest.java
+++ sandbox/src/test/java/ch/trick17/jtt/sandbox/JavassistTest.java
@@ -4,10 +4,49 @@ import javassist.*;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
+import static java.lang.String.join;
 import static java.lang.Thread.currentThread;
+import static java.util.Arrays.asList;
 
 public class JavassistTest {
+
+    sealed interface WhitelistEntry {
+        boolean matches(MethodCall m);
+    }
+
+    record WildcardEntry(String className) implements WhitelistEntry {
+        public boolean matches(MethodCall m) {
+            return m.getClassName().equals(this.className);
+        }
+    }
+
+    record MethodEntry(String className, String methodName) implements WhitelistEntry {
+        public boolean matches(MethodCall m) {
+            return m.getClassName().equals(this.className) && m.getMethodName().equals(this.methodName);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
+        var whitelist = """
+                java.lang.Math.*
+                java.lang.System.currentTimeMillis
+                """;
+
+        var parsed = whitelist.lines()
+                .map(String::trim)
+                .filter(line -> !line.isEmpty())
+                .map(line -> {
+                    var parts = asList(line.split("\\."));
+                    var className = join(".", parts.subList(0, parts.size() - 1));
+                    var methodName = parts.get(parts.size() - 1);
+                    if (parts.size() > 1 && methodName.equals("*")) {
+                        return new WildcardEntry(className);
+                    } else {
+                        return new MethodEntry(className, parts.get(parts.size() - 1));
+                    }
+                })
+                .toList();
+
         var pool = ClassPool.getDefault();
         var loader = new Loader(pool);
         loader.addTranslator(pool, new Translator() {
@@ -17,13 +56,12 @@ public class JavassistTest {
                 for (var method : cls.getDeclaredMethods()) {
                     method.instrument(new ExprEditor() {
                         public void edit(MethodCall m) throws CannotCompileException {
-                            if (m.getClassName().equals("java.lang.Math") &&
-                                m.getMethodName().equals("sin")) {
+                            if (parsed.stream().noneMatch(entry -> entry.matches(m))) {
                                 m.replace("""
                                         {
-                                            System.out.println("sin");
+                                            throw new SecurityException("Illegal method call: %s");
                                             $_ = $proceed($$);
-                                        }""");
+                                        }""".formatted(m.getClassName() + "." + m.getMethodName()));
                             }
                         }
                     });
@@ -33,8 +71,20 @@ public class JavassistTest {
         var runner = new CustomCxtClassLoaderRunner(loader);
 
         runner.call(() -> {
-            var cls = currentThread().getContextClassLoader().loadClass("ch.trick17.jtt.sandbox.ClassUnderTest");
+            var cls = currentThread().getContextClassLoader().loadClass("ch.trick17.jtt.sandbox.JavassistTest$ClassUnderTest");
             return cls.getMethod("foo").invoke(null);
         });
+    }
+
+    public static class ClassUnderTest {
+
+        public static void foo() {
+            var y = Math.sin(Math.PI);
+            System.out.println(y);
+        }
+
+        public static void main(String[] args) {
+            foo();
+        }
     }
 }
