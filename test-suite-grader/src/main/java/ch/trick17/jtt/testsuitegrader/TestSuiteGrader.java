ebf689caecb5a6a0bc9647a935375802a2c736e5
Michael Faes <rolve@trick17.ch>
2024-02-04 13:53:39 +0100

    Classify mutants based on "cause of death", including exception type and line number of assertion that failed

--- test-suite-grader/src/main/java/ch/trick17/jtt/testsuitegrader/TestSuiteGrader.java
+++ test-suite-grader/src/main/java/ch/trick17/jtt/testsuitegrader/TestSuiteGrader.java
@@ -2,6 +2,7 @@ package ch.trick17.jtt.testsuitegrader;
 
 import ch.trick17.jtt.memcompile.ClassPath;
 import ch.trick17.jtt.memcompile.InMemClassFile;
+import ch.trick17.jtt.testrunner.TestResults.MethodResult;
 import ch.trick17.jtt.testrunner.TestRunConfig;
 import ch.trick17.jtt.testrunner.TestRunner;
 import org.pitest.bytecode.analysis.ClassTree;
@@ -12,22 +13,18 @@ import org.pitest.mutationtest.engine.gregor.GregorMutater;
 import org.pitest.mutationtest.engine.gregor.config.Mutator;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Optional;
 
 import static ch.trick17.jtt.memcompile.ClassPath.empty;
 import static ch.trick17.jtt.memcompile.Compiler.JAVAC;
 import static ch.trick17.jtt.memcompile.InMemCompilation.compile;
-import static java.lang.String.join;
+import static java.util.Arrays.stream;
 import static java.util.Comparator.comparingInt;
 import static java.util.Map.Entry.comparingByKey;
 import static java.util.Map.Entry.comparingByValue;
 import static java.util.Map.entry;
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.*;
 
 public class TestSuiteGrader {
 
@@ -53,18 +50,18 @@ public class TestSuiteGrader {
         }
         var refTestSuite = refTestSuiteResult.output();
 
-        var mutants = new HashMap<List<String>, List<Mutant>>();
+        var mutants = new HashMap<Set<CauseOfDeath>, List<Mutant>>();
         try (var testRunner = new TestRunner()) {
             for (int i = 0; i < refImpls.size(); i++) {
                 var refImpl = refImpls.get(i);
                 var refResults = testRunner.run(
                         new TestRunConfig(task.testClassName(),
-                        ClassPath.fromMemory(refImpl),
-                        ClassPath.fromMemory(refTestSuite).withCurrent()));
+                                ClassPath.fromMemory(refImpl),
+                                ClassPath.fromMemory(refTestSuite).withCurrent()));
                 for (var result : refResults.methodResults()) {
                     if (!result.passed()) {
                         throw new IllegalArgumentException("Reference implementation " + (i + 1)
-                                + " failed test " + result.method(), result.exceptions().get(0));
+                                                           + " failed test " + result.method(), result.exceptions().get(0));
                     }
                 }
 
@@ -76,17 +73,16 @@ public class TestSuiteGrader {
                             task.testClassName(),
                             ClassPath.fromMemory(mutant.classes()),
                             ClassPath.fromMemory(refTestSuite).withCurrent()));
-                    var failed = mutantResults.methodResults().stream()
+                    var causes = mutantResults.methodResults().stream()
                             .filter(r -> !r.passed())
-                            .map(r -> r.method())
-                            .sorted()
-                            .toList();
-                    if (failed.isEmpty()) {
+                            .map(r -> causeOfDeath(r))
+                            .collect(toSet());
+                    if (causes.isEmpty()) {
                         System.out.println("  Warning: Mutant " + (j + 1) +
                                            " survived reference test suite" +
                                            " (" + mutant.getDescription() + ")");
                     } else {
-                        mutants.computeIfAbsent(failed, k -> new ArrayList<>()).add(mutant);
+                        mutants.computeIfAbsent(causes, k -> new ArrayList<>()).add(mutant);
                     }
                 }
             }
@@ -96,28 +92,33 @@ public class TestSuiteGrader {
         var totalMutants = mutants.values().stream().mapToInt(List::size).sum();
         System.out.println(totalMutants + " mutants killed by reference test suite\n");
 
-        System.out.println(mutants.size() + " combinations of tests killed at least one mutant:");
+        System.out.println(mutants.size() + " combinations of causes killed at least one mutant:");
         mutants.entrySet().stream()
-                .sorted(comparingByKey(comparingInt(List::size)))
-                .forEach(e -> System.out.println("  " + e.getValue().size()
-                                                 + " mutants killed by tests: "
-                                                 + join(", ", e.getKey())));
+                .sorted(comparingByKey(comparingInt(Set::size)))
+                .forEach(e -> {
+                    var count = e.getValue().size();
+                    var causes = e.getKey().stream()
+                            .map(Object::toString)
+                            .sorted()
+                            .collect(joining(", "));
+                    System.out.println("  " + count + " mutants killed by: " + causes);
+                });
         System.out.println();
 
         boolean first = true;
         while (!mutants.isEmpty()) {
-            var killsPerTest = mutants.entrySet().stream()
+            var killsPerCause = mutants.entrySet().stream()
                     .flatMap(e -> e.getKey().stream().map(t -> entry(t, e.getValue())))
                     .collect(groupingBy(Entry::getKey, summingInt(e -> e.getValue().size())));
-            var testWithMinKills = killsPerTest.entrySet().stream()
+            var causeWithMinKills = killsPerCause.entrySet().stream()
                     .filter(e -> e.getValue() > 0)
                     .min(comparingByValue()).orElseThrow()
                     .getKey();
-            var kills = killsPerTest.get(testWithMinKills);
+            var kills = killsPerCause.get(causeWithMinKills);
             System.out.println(kills + " (" + (100 * kills / totalMutants) + "%)"
                                + (!first ? " more" : "")
-                               + " mutants killed by " + testWithMinKills);
-            mutants.entrySet().removeIf(e -> e.getKey().contains(testWithMinKills));
+                               + " mutants killed by " + causeWithMinKills);
+            mutants.entrySet().removeIf(e -> e.getKey().contains(causeWithMinKills));
             first = false;
         }
     }
@@ -154,5 +155,51 @@ public class TestSuiteGrader {
             }
             return Optional.empty();
         };
+    }
+
+    private static CauseOfDeath causeOfDeath(MethodResult result) {
+        if (result.timeout()) {
+            return new Timeout(result.method());
+        } else if (result.outOfMemory()) {
+            return new OutOfMemory(result.method());
+        } else if (!result.illegalOps().isEmpty()) {
+            return new IllegalOp(result.method(), result.illegalOps().get(0));
+        } else {
+            var e = result.exceptions().get(0);
+            var line = stream(e.getStackTrace())
+                    .filter(s -> s.getMethodName().equals(result.method()))
+                    .findFirst().orElseThrow()
+                    .getLineNumber();
+            return new Exception(result.method(), line, e.getClass());
+        }
+    }
+
+    sealed interface CauseOfDeath {
+        String method();
+    }
+
+    public record Timeout(String method) implements CauseOfDeath {
+        public String toString() {
+            return method + " (timeout)";
+        }
+    }
+
+    public record OutOfMemory(String method) implements CauseOfDeath {
+        public String toString() {
+            return method + " (out of memory)";
+        }
+    }
+
+    public record IllegalOp(String method, String op) implements CauseOfDeath {
+        public String toString() {
+            return method + " (illegal op: " + op + ")";
+        }
+    }
+
+    public record Exception(String method, int line,
+                            Class<?> exceptionClass) implements CauseOfDeath {
+        public String toString() {
+            return method + ":" + line + " (" + exceptionClass.getSimpleName() + ")";
+        }
     }
 }
